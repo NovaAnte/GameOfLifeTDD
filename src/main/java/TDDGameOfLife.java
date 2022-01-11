63be82d161d45d269043c37209cba8e5136253e3
NovaAnte <andreas_cabraja@hotmail.com>
2022-01-11 16:17:58 +0100

    Created the ability to initialize grid and cleared test for grid length and height

--- src/main/java/TDDGameOfLife.java
+++ src/main/java/TDDGameOfLife.java
@@ -1,2 +1,12 @@
 public class TDDGameOfLife {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String [][] initializeGrid(int y, int x){
+        String board[][] = new String [y][x];
+        return board;
+    }
+
 }
--- src/test/java/TDDGameOfLifeTest.java
+++ src/test/java/TDDGameOfLifeTest.java
@@ -3,15 +3,22 @@ import org.junit.jupiter.api.Test;
 
 public class TDDGameOfLifeTest {
 
+    private final TDDGameOfLife tddgameoflife;
+
+    public TDDGameOfLifeTest() {
+        this.tddgameoflife = new TDDGameOfLife();
+    }
+
     @Test
     void initializeGridWithXisEightandYisFour(){
         // Arrange
         int x = 8;
         int y = 4;
         // Act
-        String spelplan [] [] = initializeGrid(x, y);
+        String spelplan [] [] = tddgameoflife.initializeGrid(y, x);
         // Assert
-        Assertions.assertEquals(8, spelplan.length);
+        Assertions.assertEquals(4, spelplan.length);
+        Assertions.assertEquals(8, spelplan[0].length);
     }
 
     // Kontrollera att griden är 8 enheter bred och 4 enheter lång.
