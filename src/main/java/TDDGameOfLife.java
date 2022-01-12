216a509ff3d420b8d976aad00493b95264662413
NovaAnte <andreas_cabraja@hotmail.com>
2022-01-12 18:28:19 +0100

    Implemented code to pass  bottom edge cases

--- src/main/java/TDDGameOfLife.java
+++ src/main/java/TDDGameOfLife.java
@@ -117,6 +117,19 @@ public class TDDGameOfLife {
             list.add(pos5);
         }
 
+        if (x > 0 && x < this.boardLength -1 && y == this.boardHeight -1){ // Bottom edge
+            ImmutablePair<Integer, Integer> pos1 = new ImmutablePair<>(x-1, y);
+            ImmutablePair<Integer, Integer> pos2 = new ImmutablePair<>(x-1, y-1);
+            ImmutablePair<Integer, Integer> pos3 = new ImmutablePair<>(x, y-1);
+            ImmutablePair<Integer, Integer> pos4 = new ImmutablePair<>(x+1, y-1);
+            ImmutablePair<Integer, Integer> pos5 = new ImmutablePair<>(x+1, y);
+            list.add(pos1);
+            list.add(pos2);
+            list.add(pos3);
+            list.add(pos4);
+            list.add(pos5);
+        }
+
         //ImmutablePair<Integer, Integer> pos1 = new ImmutablePair<>(x-1, y-1);
         //ImmutablePair<Integer, Integer> pos2 = new ImmutablePair<>(x, y-1);
         //ImmutablePair<Integer, Integer> pos3 = new ImmutablePair<>(x+1, y-1);
