809fb31285b9bee3c10faf217ae88baf95796536
NovaAnte <andreas_cabraja@hotmail.com>
2022-01-12 17:04:47 +0100

    Added test for right edge case with three alive neighbors.

--- src/test/java/TDDGameOfLifeTest.java
+++ src/test/java/TDDGameOfLifeTest.java
@@ -457,4 +457,19 @@ public class TDDGameOfLifeTest {
         assertEquals(2, result);
     }
 
+    @Test
+    void decideIfCellLivesOrDies_WhenAliveCellIsRightEdgeCaseWithThreeNeighbors_ReturnThree(){
+        // Arrange
+        this.tddgameoflife.initializeBoard(5, 8);
+        this.tddgameoflife.populateBoard();
+        this.tddgameoflife.initializeStartingPoint(2,7);
+        this.tddgameoflife.initializeStartingPoint(1,7);
+        this.tddgameoflife.initializeStartingPoint(3,7);
+        this.tddgameoflife.initializeStartingPoint(3,6);
+        // Act
+        int result = this.tddgameoflife.decideIfCellLivesOrDies(7,2);
+        // Assert
+        assertEquals(3, result);
+    }
+
 }
